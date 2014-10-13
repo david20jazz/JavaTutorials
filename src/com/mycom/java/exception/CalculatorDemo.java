4fa81f75e219dcc1d4a37f970e3e47cf65fce74c
david20jazz <david20jazz@gmail.com>
2014-10-13 18:06:58 +0900

    예외검사 try,catch 사용법

--- src/com/mycom/java/exception/CalculatorDemo.java
+++ src/com/mycom/java/exception/CalculatorDemo.java
@@ -8,15 +8,34 @@ class Calculator{
 	}
 	public void divide(){
 		/*출력결과
-		계산결과는 오류가 발생했습니다 : / by zero
+		계산결과는 
+
+		e.getMessage()
+		/ by zero
+		
+		
+		e.toString()
+		java.lang.ArithmeticException: / by zero
+		
+		
+		e.printStackTrace()
+		java.lang.ArithmeticException: / by zero
+			at com.mycom.java.exception.Calculator.divide(CalculatorDemo.java:15)
+			at com.mycom.java.exception.CalculatorDemo.main(CalculatorDemo.java:32)
+		
+		divide End
 		*/
 		try {
 			System.out.print("계산결과는 ");
 			System.out.print(this.left/this.right);
 			System.out.print(" 입니다.");
 		} catch(Exception e){
-			System.out.println("오류가 발생했습니다 : "+e.getMessage());
+			System.out.println("\n\ne.getMessage()\n"+ e.getMessage());
+			System.out.println("\n\ne.toString()\n"+ e.toString());
+			System.out.println("\n\ne.printStackTrace()");
+			e.printStackTrace();
 		}
+		System.out.println("divide End");
 	}
 }
 
